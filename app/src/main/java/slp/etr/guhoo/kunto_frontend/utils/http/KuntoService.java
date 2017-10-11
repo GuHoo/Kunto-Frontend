dc22e45474eda10de4d5ef6a70fb2df6dec3801e
Nakai <s12t282@stmail.eng.kagawa-u.ac.jp>
2017-10-11 17:46:50 +0900

    タイポ修正

--- app/src/main/java/slp/etr/guhoo/kunto_frontend/utils/http/KuntoService.java
+++ app/src/main/java/slp/etr/guhoo/kunto_frontend/utils/http/KuntoService.java
@@ -7,7 +7,7 @@ import slp.etr.guhoo.kunto_frontend.utils.http.entity.SignInResponse;
 import slp.etr.guhoo.kunto_frontend.utils.http.entity.SignUpResponse;
 
 public interface KuntoService {
-    @POST("api/users/sign_in")
+    @POST("api/users/sign_up")
     Observable<SignUpResponse> signUp(@Query("email") String email, @Query("password") String password, @Query("password_confirmation") String password_confirmation);
 
     @POST("api/users/sign_in")
