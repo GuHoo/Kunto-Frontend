61181ccd7f4f6b06fdbfcb38b084a4ba6462c188
Nakai <s12t282@stmail.eng.kagawa-u.ac.jp>
2017-10-13 16:28:38 +0900

    トップページアクティビティ作成

---
+++ app/src/main/java/slp/etr/guhoo/kunto_frontend/activitys/TopPageActivity.java
@@ -0,0 +1,15 @@
+package slp.etr.guhoo.kunto_frontend.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import slp.etr.guhoo.kunto_frontend.R;
+
+public class TopPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_page);
+    }
+}
