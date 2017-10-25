1b2bdf885b28c3f118ad2c20ce9433687f0ae51a
Nakai <s12t282@stmail.eng.kagawa-u.ac.jp>
2017-10-25 18:25:40 +0900

    トップページ作成

--- app/src/main/java/slp/etr/guhoo/kunto_frontend/activitys/TopPageActivity.java
+++ app/src/main/java/slp/etr/guhoo/kunto_frontend/activitys/TopPageActivity.java
@@ -1,15 +1,32 @@
 package slp.etr.guhoo.kunto_frontend.activitys;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import slp.etr.guhoo.kunto_frontend.R;
 
-public class TopPageActivity extends AppCompatActivity {
+public class TopPageActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_page);
+        findViewById(R.id.buttonSignIn).setOnClickListener(this);
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonSignIn) {
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.buttonSignUp) {
+            /*
+            Intent intent = new Intent(this, SignUpActivity.class);
+            startActivity(intent);
+            */
+        }
     }
 }
